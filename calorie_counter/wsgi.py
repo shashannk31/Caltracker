55b5eeabab2b7ad7870a1c4b0f484edb542a7393
shashannk31 <shashannk2004@gmail.com>
2023-09-20 11:10:44 +0530

    for deployement

--- calorie_counter/wsgi.py
+++ calorie_counter/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'calorie_counter.settings')
 
 application = get_wsgi_application()
+
+app = application
